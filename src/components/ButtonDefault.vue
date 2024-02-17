17d534030f2f9d05fd7364d255e522fd075f5757
Elian-beep <elian.19batista@gmail.com>
2024-02-17 10:46:55 -0300

    [PROJECTS] add new project: 'curriculo-de-bolso'

---
+++ src/components/ButtonDefault.vue
@@ -0,0 +1,58 @@
+<template>
+    <button :class="model" @click="functionClick()">
+        {{ text }}
+    </button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    name: 'ButtonDefault',
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        functionClick: {
+            type: Function,
+            required: true
+        },
+        model: {
+            type: String,
+            required: true,
+        }
+    }
+}); 
+</script>
+
+<style scoped>
+@import '@/assets/main.css';
+
+button{
+    min-width: 100%;
+    padding: 16px;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: 600;
+    color: var(--color-blue);
+    /* z-index: 99;    */
+    transition: all .2s ease-in;
+}
+
+.outline{
+    border: 1px solid var(--color-orange);
+    background: var(--color-white);
+}
+
+.solid{
+    background: var(--color-orange-min);
+    border: 1px solid var(--color-orange-min);
+}
+
+button:hover{
+    background: var(--color-orange);
+}
+
+</style>
